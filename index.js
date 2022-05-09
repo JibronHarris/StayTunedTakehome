91037b8d6416f1558c0f6df64396518d17637173
JibronHarris <Jibron.Harris@gmail.com>
2022-05-09 16:16:00 -0400

    restarted project with create-react-app for frontend

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const route = require('./src/api/routes');
+const app = express();
+const port = 8000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
+
+app.use('/', route);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const route = require('./api/routes');
-const app = express();
-const port = 3000;
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(bodyParser.raw());
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-app.use('/', route);
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
