4c5073a3ffd3266d733aa44f7db96f2d10c175d5
JibronHarris <Jibron.Harris@gmail.com>
2022-05-10 00:17:29 -0400

    modified index.js

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const route = require('./src/api/routes');
 const app = express();
+const path = require('path');
 const port = process.env.PORT || 8000;
 
 app.use(cors());
+
+app.use(express.static(path.resolve(__dirname, './client/build')));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
